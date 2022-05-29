e53e0ab0be472e7e3b455b491cb147a6f9fcf194
Nguyen811 <trinhhoainguyenn@gmail.com>
2022-05-29 22:33:46 +0700

    Add project files.

---
+++ PizzaPlace/Client/Pages/CustomerEntry.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using PizzaPlace.Shared;
+
+namespace PizzaPlace.Client.Pages
+{
+    partial class CustomerEntry
+    {
+        [Parameter]
+        public string Title { get; set; } = default!;
+        [Parameter]
+        public string ButtonTitle { get; set; } = default!;
+        [Parameter]
+        public string ButtonClass { get; set; } = default!;
+        [Parameter]
+        public Customer Customer { get; set; } =default!;
+        [Parameter]
+        public EventCallback ValidSubmit { get; set; } = default!;
+        [Parameter]
+        public EventCallback<Customer> CustomerChanged { get; set; }
+        private void FieldChanged(string fieldName)
+        {
+            CustomerChanged.InvokeAsync(Customer);
+            isInvalid = !inputWather.Validate();
+        }
+        private InputWather inputWather= default!;
+        bool isInvalid = true;
+    }
+}
---
+++ PizzaPlace/Shared/UI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PizzaPlace.Shared
+{
+    public class UI
+    {
+        public bool ShowBasket { get; set; } = true;
+    }
+}
